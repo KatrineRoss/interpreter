39ea2a36f281c16b47cfc711af325478b679dc86
Ekaterina Egorova <e.egorova@accenture.com>
2021-07-14 09:01:36 +0300

    bugs-fix

--- script.js
+++ script.js
@@ -2,9 +2,70 @@ const resultField = document.getElementsByClassName('result-field')?.[0];
 const clearBtn = document.getElementsByClassName('expression-input__clear-btn')?.[0];
 
 const isFirstOrderOperationSign = (char) => '/*'.includes(char);
+const isSecondOrderOperationSign = (char) => '-+'.includes(char);
 const isOperationSign = (char) => '/*+-'.includes(char);
 
 const isInteger = (char) => '0123456789'.includes(char);
+
+const inverseExpression = (expression) => {
+    let result = '';
+    const isNegative = expression?.[0] === '-';
+
+    for (index in expression) {
+        switch(expression[index]) {
+            case '+':
+                result += '-';
+                break;
+            case '-':
+                result += '+';
+                break;
+            default:
+                result += expression[index];
+        }
+    }
+
+    return isNegative ? result.slice(1) : `-${result}`;
+}
+
+const findEndOfFirstOrderOperationIndex = (expression, ) => {
+    let closedParen = true;
+
+    for (let i = 0; i < expression.length; i++) {
+        if (expression[i] === '(') {
+            closedParen = false;
+        }
+
+        if (expression[i] === ')') {
+            closedParen = true;
+        }
+
+        if (isSecondOrderOperationSign(expression[i]) && closedParen) {
+            return i;
+        }
+    }
+
+    return -1;
+};
+
+const findEndOfSubExpressionIndex = (expression, ) => {
+    let nestedExpressionNotClosed = 0;
+
+    for (let i = 0; i < expression.length; i++) {
+        if (expression[i] === '(') {
+            nestedExpressionNotClosed++;
+        }
+
+        if (expression[i] === ')' && nestedExpressionNotClosed === 1) {
+            return i;
+        }
+
+        if (expression[i] === ')') {
+            nestedExpressionNotClosed--;
+        }
+    }
+
+    return -1;
+};
 
 const ERRORS_TYPES = Object.freeze({
     NEED_OPERATION_SIGN: 'NEED_OPERATION_SIGN',
@@ -47,8 +108,8 @@ const TOKEN_TYPES = Object.freeze({
 });
 
 class Token {
-    constructor(text, type) {
-        this.text = text;
+    constructor(value, type) {
+        this.value = value;
         this.type = type;
     }
 }
@@ -109,9 +170,10 @@ class ExpressionProcessor {
         const tokens = [];
 
         for (let i = 0; i < expression.length; i++) {
+            const isNegative = expression[i-1] === TOKEN_TYPES.MINUS;
+
             if (isInteger(expression[i])) {
                 const num = [];
-                const isNegative = expression[i-1] === TOKEN_TYPES.MINUS;
 
                 while (isInteger(expression[i])) {
                     num.push(expression[i]);
@@ -119,34 +181,45 @@ class ExpressionProcessor {
                 }
 
                 if (!isFirstOrder && isFirstOrderOperationSign(expression[i])) {
-                    const subExpression = expression.slice(i - num.length);
-                    const firstSecOrderOperationSignIndex = subExpression.search(/\+|-/);
+                    let subExpression = expression.slice(i - num.length);
+                    // Firstly, look for 
+                    // const firstRParenIndex = subExpression.search(/)/);
+                    // const firstSecOrderOperationSignIndex = subExpression.search(/\+|-/);
+                    // const endOfSubExpressionIndex = firstRParenIndex >= 0 ? firstRParenIndex : firstSecOrderOperationSignIndex;
+                    const endOfSubExpressionIndex = findEndOfFirstOrderOperationIndex(subExpression);
+                    
+                    subExpression = subExpression.slice(0, endOfSubExpressionIndex >= 0 ? endOfSubExpressionIndex : undefined);
 
-                    tokens.push(this._parse(subExpression.slice(0, firstSecOrderOperationSignIndex >= 0 ? firstSecOrderOperationSignIndex : undefined), true));
+                    tokens.push(this._parse(isNegative ? `-${subExpression}` : subExpression, true));
+                    // tokens.push(this._parse(subExpression, true));
 
-                    i = firstSecOrderOperationSignIndex >= 0 ? firstSecOrderOperationSignIndex : expression.length;
+                    i = endOfSubExpressionIndex >= 0 ? endOfSubExpressionIndex + i - 1 : expression.length;
                 } else {
                     const avgNum = num.join('');
                     tokens.push(new Token(isNegative ? `-${avgNum}` : avgNum, TOKEN_TYPES.INTEGER));
+                    // tokens.push(new Token(avgNum, TOKEN_TYPES.INTEGER));
                 }
             }
 
-            if (isOperationSign(expression[i])) {
+            if (expression[i-1] && isOperationSign(expression[i])) {
                 // if operation sign is minus replace it by pluse, because we already know if the integer is negative.
                 const operation = expression[i] === TOKEN_TYPES.MINUS ? TOKEN_TYPES.PLUS : expression[i];
+                // const operation = expression[i];
 
                 tokens.push(new Token(operation, operation));
             }
 
             if (expression[i] === TOKEN_TYPES.PAREN_L) {
-                const parenRightIndex = expression.slice(i).search(TOKEN_TYPES.PAREN_R);
+                // const parenRightIndex = expression.slice(i).search(/\)/);
+                const parenRightIndex = findEndOfSubExpressionIndex(expression.slice(i));
 
-                if (!parenRightIndex) {
+                if (parenRightIndex === -1) {
                     throw new Error(getErrorMessage(ERRORS_TYPES.NEED_CLOSING_PARENTHESIS, expression[i]));
                 } else {
-                    tokens.push(this._parse(expression.slice(i + 1, parenRightIndex - 1)));
+                    const subExpression = expression.slice(i + 1, parenRightIndex + i);
+                    tokens.push(this._parse(isNegative ? inverseExpression(subExpression) : subExpression));
 
-                    i = parenRightIndex + 1;
+                    i = parenRightIndex + i;
                 }
             }
         }
@@ -158,7 +231,7 @@ class ExpressionProcessor {
         const hasSubExpression = tokens.some((token) => Array.isArray(token));
         const flatedTokensList = hasSubExpression ? tokens.map((token) => {
             if (Array.isArray(token)) {
-                return new Integer(this._process(token));
+                return this._process(token);
             }
 
             return token;
@@ -168,15 +241,15 @@ class ExpressionProcessor {
         const operationSignIndex = index + 1;
 
         if (index >= flatedTokensList.length - 1) {
-            return new Integer(flatedTokensList[leftArgIndex]?.text);
+            return new Integer(flatedTokensList[leftArgIndex]?.value);
         }
 
         /**
          * If has an operation sign [1] and a left arg [2] - create binary operation and process it recursively.
          */
         if (flatedTokensList[operationSignIndex]) {
-            if (!isInteger(flatedTokensList[operationSignIndex].text)) {
-                const bo = new BinaryOperation(new Integer(flatedTokensList[leftArgIndex]?.text), this._process(flatedTokensList, rightArgIndex), flatedTokensList[operationSignIndex].text);
+            if (!isInteger(flatedTokensList[operationSignIndex].value)) {
+                const bo = new BinaryOperation(new Integer(flatedTokensList[leftArgIndex]?.value), this._process(flatedTokensList, rightArgIndex), flatedTokensList[operationSignIndex].value);
 
                 return new Integer(bo.value);
             }
